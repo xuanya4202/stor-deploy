8f41fc623674e870c458b046c79dda70d06f015e
Yuanguo <yuanguo_huo@163.com>
2017-06-30 16:13:50 +0000

    dev

--- tools.sh
+++ tools.sh
@@ -17,6 +17,120 @@ function log()
 
 ##################### mount-tool #####################
 
+function umount_dev()
+{
+    local node=$1
+    local user=$2
+    local ssh_port=$3
+    local device=$4
+    local mntpoint=$5
+
+    log "INFO: Enter umount_dev(): node=$node user=$user ssh_port=$ssh_port"
+    log "INFO:     device=$device"
+    log "INFO:     mntpoint=$mntpoint"
+
+    local SSH="ssh -p $ssh_port $user@$node"
+    local sshErr=`mktemp --suffix=-stor-deploy.umount`
+
+    $SSH umount $device > $sshErr 2>&1
+    if [ -s $sshErr ] ; then
+        cat $sshErr | grep "not mounted" > /dev/null 2>&1
+        if [ $? -ne 0 ] ; then   # didn't found 'not mounted'
+            log "ERROR: Exit umount_dev(): failed to umount $device. See $sshErr for details"
+            return 1
+        fi
+    fi
+
+    $SSH umount $mntpoint  > $sshErr 2>&1 
+    if [ -s $sshErr ] ; then
+        cat $sshErr | grep "not mounted" > /dev/null 2>&1
+        if [ $? -ne 0 ] ; then   # didn't found 'not mounted'
+            log "ERROR: Exit umount_dev(): failed to umount $mntpoint See $sshErr for details"
+            return 1
+        fi
+    fi
+
+    #remove the device from /fstab
+    $SSH sed -i -e "\#$device#d" /etc/fstab 2> $sshErr
+    if [ -s $sshErr ] ; then
+        log "ERROR: Exit umount_dev(): failed to remove $device from fstab. See $sshErr for details"
+        return 1
+    fi
+
+    #remove the uuid of the device from /fstab
+    local uuid=`$SSH blkid $device 2> $sshErr | grep -w UUID | sed -e 's/.*\<UUID\>="\([-0-9a-fA-F]*\)".*/\1/'`
+    if [ -s $sshErr ] ; then
+        log "ERROR: Exit umount_dev(): failed to get uuid of $device on $node. See $sshErr for details"
+        return 1
+    fi
+
+    if [ -n "$uuid" ] ; then
+        $SSH sed -i -e "/$uuid/d" /etc/fstab 2> $sshErr
+        if [ -s $sshErr ] ; then
+            log "ERROR: Exit umount_dev(): failed to remove uuid ($uuid) of $device from fstab. See $sshErr for details"
+            return 1
+        fi
+    fi
+
+    rm -f $sshErr
+
+    log "INFO: Exit umount_dev(): Success"
+    return 0
+}
+
+function do_mounts()
+{
+    local node=$1
+    local user=$2
+    local ssh_port=$3
+    local mounts=$4
+    local mount_opts=$5
+    local mkfs_cmd=$6
+
+    log "INFO: Enter do_mounts(): node=$node user=$user ssh_port=$ssh_port"
+    log "INFO:     mounts=$mounts"
+    log "INFO:     mount_opts=$mount_opts"
+    log "INFO:     mkfs_cmd=$mkfs_cmd"
+
+    local SSH="ssh -p $ssh_port $user@$node"
+    local sshErr=`mktemp --suffix=-stor-deploy.domounts`
+
+    while [ -n "$mounts" ] ; do
+        local one_mnt=""
+
+        echo "$mounts" | grep "," > /dev/null 2>&1
+        if [ $? -eq 0 ] ; then     #',' is found
+            one_mnt=`echo $mounts | cut -d ',' -f 1`
+            mounts=`echo $mounts | cut -d ',' -f 2-`
+
+            one_mnt=`echo $one_mnt | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+            mounts=`echo $mounts | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+        else
+            one_mnt=$mounts
+            mounts=""
+        fi
+
+        local device=`echo $one_mnt | cut -d ':' -f 1`
+        local mntpoint=`echo $one_mnt | cut -d ':' -f 2`
+
+        device=`echo $device | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+        mntpoint=`echo $mntpoint | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+
+        if [ -z "$device" -o -z "$mntpoint" ] ; then
+            log "ERROR: Exit do_mounts(): mounts is invalid: mounts=$mounts device=$device mntpoint=$mntpoint"
+            return 1
+        fi
+
+    done
+
+    rm -f $sshErr
+
+    log "INFO: Exit do_mounts(): Success"
+    return 0
+}
+
+
+umount_dev 192.168.100.132 root 22 /dev/sdd1 /home/watermelon
 
 
 
--- zk-deploy.sh
+++ zk-deploy.sh
@@ -11,28 +11,29 @@ ZKSCRIPT_PDIR="$(cd "${ZKSCRIPT_PDIR}"; pwd)"
 
 function clean_up_zknode()
 {
-    #only root user is supported, so we don't pass user as parameter;
+    #user must be 'root' for now, so we don't use sudo;
     local node=$1
-    local ssh_port=$2
-    local install_path=$3
-    local version=$4
+    local user=$2
+    local ssh_port=$3
+    local install_path=$4
+    local version=$5
 
-    log "INFO: Enter function clean_up_zknode(): node=$node ssh_port=$ssh_port install_path=$install_path version=$version"
+    log "INFO: Enter clean_up_zknode(): node=$node user=$user ssh_port=$ssh_port install_path=$install_path version=$version"
 
-    local SSH="ssh -p $port $node"
+    local SSH="ssh -p $port $user@$node"
     local sshErr=`mktemp --suffix=-stor-deploy.ssh.err`
     local ssh_cmd=""
 
-    # Step-1: try to stop running zookeeper process if found.
+    #Step-1: try to stop running zookeeper process if found.
     ssh_cmd="$SSH jps 2> $sshErr | grep QuorumPeerMain | cut -d ' ' -f 1 | grep '^[0-9][0-9]*$'"
     local zk_pid=`$ssh_cmd`
     if [ -s "$sshErr" ] ; then
-        log "ERROR: Exit function clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
+        log "ERROR: Exit clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
         return 1
     fi
 
     if [ -n "$zk_pid" ] ; then  # we found a zookeeper process;
-        log "INFO: in function clean_up_zknode(): try all ways to stop zookeeper"
+        log "INFO: in clean_up_zknode(): try all ways to stop zookeeper"
         $SSH systemctl stop zookeeper
         sleep 2
         $SSH kill -9 $zk_pid
@@ -40,30 +41,96 @@ function clean_up_zknode()
 
         zk_pid=`$ssh_cmd`
         if [ -s "$sshErr" ] ; then
-            log "ERROR: Exit function clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
+            log "ERROR: Exit clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
             return 1
         fi
 
         if [ -n "$zk_pid" ] ; then
-            log "ERROR: Exit function clean_up_zknode(): found a zookeeper on $onde and failed to stop it"
+            log "ERROR: Exit clean_up_zknode(): found a zookeeper on $onde and failed to stop it"
             return 1
         else
-            log "INFO: Exit function clean_up_zknode(): succeeded to stop zookeeper on $onde"
+            log "INFO: Exit clean_up_zknode(): succeeded to stop zookeeper on $onde"
         fi
     fi
 
-    # Step-2: try to remove the zookeeper package;
-    ssh_cmd="$SSH rm -fr $install_path/$version 2> $sshErr"
+    #Step-2: try to remove the zookeeper package;
+    $SSH systemctl disable zookeeper
+    $SSH rm -fr $install_path/$version /usr/lib/systemd/system/zookeeper.service /etc/systemd/system/zookeeper.service
+
+    ssh_cmd="$SSH ls $install_path/$version /usr/lib/systemd/system/zookeeper.service /etc/systemd/system/zookeeper.service > $sshErr 2>&1"
     $ssh_cmd
-    if [ -s "$sshErr" ] ; then
-        log "ERROR: Exit function clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
+    local n=`cat $sshErr | grep "No such file or directory" | wc -l`
+    if [ $n -ne 3 ] ; then
+        log "ERROR: Exit clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
         return 1
     fi
 
-    # Step-3: try to remove systemctl config;
-    
+    ssh_cmd="$SSH systemctl daemon-reload" 2> $sshErr
+    $ssh_cmd
+    if [ -s "$sshErr" ] ; then
+        log "ERROR: Exit clean_up_zknode(): ssh failed. ssh_cmd=\"$ssh_cmd\". See $sshErr for details"
+        return 1
+    fi
 
-    log "INFO: Exit function clean_up_zknode(): Success"
+    rm -f $sshErr
+
+    log "INFO: Exit clean_up_zknode(): Success"
+    return 0
+}
+
+function prepare_zknode()
+{
+    #user must be 'root' for now, so we don't use sudo;
+    local node=$1
+    local user=$2
+    local ssh_port=$3
+    local java_home=$4
+    local dataDir=$5
+    local dataLogDir=$6
+    local logdir=$7
+    local pidfile=$8
+
+    shift 8
+
+    local mounts=$1
+    local mount_opts=$2
+    local mkfs_cmd=$3
+
+    log "INFO: Enter prepare_zknode(): node=$node user=$user ssh_port=$ssh_port"
+    log "INFO:     java_home=$java_home"
+    log "INFO:     dataDir=$dataDir"
+    log "INFO:     dataLogDir=$dataLogDir"
+    log "INFO:     logdir=$logdir"
+    log "INFO:     pidfile=$pidfile"
+    log "INFO:     mounts=$mounts"
+    log "INFO:     mount_opts=$mount_opts"
+    log "INFO:     mkfs_cmd=$mkfs_cmd"
+
+    local SSH="ssh -p $port $user@$node"
+    local sshErr=`mktemp --suffix=-stor-deploy.ssh.err`
+    local ssh_cmd=""
+
+    #Step-1: test if java is available
+    $SSH $java_home/bin/java -version > $sshErr 2>&1
+    cat $sshErr | grep "java version" > /dev/null 2>&1
+    if [ $? -ne 0 ] ;  then  #didn't found "java version", so java is not available
+        log "ERROR: Exit prepare_zknode(): java is not availabe at $java_home on $node. See $sshErr for details"
+        return 1
+    fi
+
+    #Step-2: mount the disk
+    if [ -n "$mounts" ] ; then
+        if [ -z "$mount_opts" -o -z "$mkfs_cmd" ] ; then
+            log "ERROR: Exit prepare_zknode(): mount_opts and mkfs_cmd must be present when mounts is present"
+            return 1
+        fi
+
+        log "INFO: in prepare_zknode(): "
+    fi
+
+    rm -f $sshErr
+
+    log "INFO: Exit prepare_zknode(): Success"
     return 0
 }
 
@@ -71,19 +138,19 @@ function deploy_zk()
 {
     local parsed_conf_dir=$1
 
-    log "INFO: Enter function deploy_zk(): parsed_conf_dir=$parsed_conf_dir"
+    log "INFO: Enter deploy_zk(): parsed_conf_dir=$parsed_conf_dir"
 
     local zk_conf_dir=$parsed_conf_dir/zk
     local zk_comm_cfg=$zk_conf_dir/common
     local zk_nodes=$zk_conf_dir/nodes
 
     if [ ! -d $zk_conf_dir ] ; then
-        log "ERROR: Exit function deploy_zk(): dir $zk_conf_dir does not exist"
+        log "ERROR: Exit deploy_zk(): dir $zk_conf_dir does not exist"
         return 1
     fi
 
     if [ ! -f $zk_comm_cfg -o ! -f $zk_nodes ] ; then
-        log "ERROR: Exit function deploy_zk(): file $zk_comm_cfg or $zk_nodes does not exist"
+        log "ERROR: Exit deploy_zk(): file $zk_comm_cfg or $zk_nodes does not exist"
         return 1
     fi
 
@@ -104,27 +171,34 @@ function deploy_zk()
         local pidfile=`grep "env:ZOOPIDFILE=" $node_cfg | cut -d '=' -f 2-`
         local logdir=`grep "env:ZOO_LOG_DIR=" $node_cfg | cut -d '=' -f 2-`
 
-        log "INFO: in function deploy_zk(): node=$node node_cfg=$node_cfg"
-        log "INFO: in function deploy_zk():         user=$user"
-        log "INFO: in function deploy_zk():         ssh_port=$ssh_port"
-        log "INFO: in function deploy_zk():         java_home=$java_home"
-        log "INFO: in function deploy_zk():         install_path=$install_path"
-        log "INFO: in function deploy_zk():         version=$version"
-        log "INFO: in function deploy_zk():         mounts=$mounts"
-        log "INFO: in function deploy_zk():         mkfs_cmd=$mkfs_cmd"
-        log "INFO: in function deploy_zk():         mount_opts=$mount_opts"
-        log "INFO: in function deploy_zk():         dataDir=$dataDir"
-        log "INFO: in function deploy_zk():         dataLogDir=$dataLogDir"
-        log "INFO: in function deploy_zk():         pidfile=$pidfile"
-        log "INFO: in function deploy_zk():         logdir=$logdir"
+        log "INFO: in deploy_zk(): node=$node node_cfg=$node_cfg"
+        log "INFO: in deploy_zk():         user=$user"
+        log "INFO: in deploy_zk():         ssh_port=$ssh_port"
+        log "INFO: in deploy_zk():         java_home=$java_home"
+        log "INFO: in deploy_zk():         install_path=$install_path"
+        log "INFO: in deploy_zk():         version=$version"
+        log "INFO: in deploy_zk():         mounts=$mounts"
+        log "INFO: in deploy_zk():         mkfs_cmd=$mkfs_cmd"
+        log "INFO: in deploy_zk():         mount_opts=$mount_opts"
+        log "INFO: in deploy_zk():         dataDir=$dataDir"
+        log "INFO: in deploy_zk():         dataLogDir=$dataLogDir"
+        log "INFO: in deploy_zk():         pidfile=$pidfile"
+        log "INFO: in deploy_zk():         logdir=$logdir"
 
         if [ X"$user" != "root" ] ; then
             log "ERROR: currently, only 'root' user is allowed."
             return 1
         fi
 
-        log "INFO: in function deploy_zk(): clean up zk node $node ..."
-        clean_up $node $ssh_port $install_path $version
+        log "INFO: in deploy_zk(): clean up zk node $node ..."
+        clean_up_zknode "$node" "$user" "$ssh_port" "$install_path" "$version"
+        if [ $? -ne 0 ] ; then
+            log "ERROR: Exit deploy_zk(): failed to clean up zk node $node"
+        fi
+
+        prepare_zknode "$node" "$user" "$ssh_port" "$java_home" "$dataDir" "$dataLogDir" "$logdir" "$pidfile" "$mounts" "$mount_opts" "$mkfs_cmd"
+
+
     done
 }
 
